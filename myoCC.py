79c896af2e6662d68ae143121673e23fe71bb6c6
Hjalte B Nielsen <hjalte33@gmail.com>
2016-10-20 15:47:21 +0200

    code still in progress

--- myoCC.py
+++ myoCC.py
@@ -1,7 +1,20 @@
 from modules import ccctrl
 driver=ccctrl.dynamixel_mx(ttyUSB0)
 
-homePosition=[]
-while :
+#lock all motors so it's not just spagetti
+driver.torque_enable(254)
+
+#get the robot slowly to it's home position.
+homePosition=[2048,2048,2048,2048,2048]
+bool ishome = False
+howFarGoal =[]
+for n in xrange(0,4):
+	howFarGoal.append(n)=driver.get_position(n+1)-homePosition[n]
+	pass
+	
+while ishome != True:
+	for x in xrange(0,4):
+		
+		pass
 	pass
 
