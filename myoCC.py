0f062d2fbd0bf03c773f4407db225d8c6671a736
Hjalte Nielsen <Hjalte Nielsen>
2016-10-24 21:33:22 +0200

    made the arm go to home position. compiles with no errors. not tested

--- myoCC.py
+++ myoCC.py
@@ -1,20 +1,39 @@
-from modules import ccctrl
-driver=ccctrl.dynamixel_mx(ttyUSB0)
+import time
 
-#lock all motors so it's not just spagetti
+from modules import ccctrl
+driver=ccctrl.dynamixel_mx('/dev/ttyUSB0') 
+#on windows write the com port as 'COM9' if its port 9.
+
+#lock all motors so it's not just a spagetti robot
 driver.torque_enable(254)
 
 #get the robot slowly to it's home position.
-homePosition=[2048,2048,2048,2048,2048]
-bool ishome = False
+homePosition=[2048,1024,2048,2048,2048]
 howFarGoal =[]
+
+#how far are we from home?
 for n in xrange(0,4):
-	howFarGoal.append(n)=driver.get_position(n+1)-homePosition[n]
-	pass
-	
-while ishome != True:
+	howFarGoal.append(driver.get_position(n+1)-homePosition[n])
+
+# Send trajectory 	
+isHome = False
+while isHome != True:
 	for x in xrange(0,4):
+		setPoint = homePosition[x] + howFarGoal[x]
+		if howFarGoal[x] > 0:
+			howFarGoal -= 1 
+		elif howFarGoal < 0:
+			howFarGoal += 1
+		driver.set_position(x+1, setPoint)
 		
-		pass
-	pass
+	isHome = True
+	#check if we are done
+	for x in xrange(0,4):
+		if howFarGoal[x] != 0:
+			isHome = False
+			break
+	time.sleep(5)	
+	
+	
+	
 
